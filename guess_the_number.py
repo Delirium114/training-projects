2a2417fb2b4798a3b5b5f8c4fb5fa626b54d8c5c
Bilal Khasuev <khasuev.bilal@mail.ru>
2025-05-05 18:21:56 +0300

    Update guess_the_number.py

--- guess_the_number.py
+++ guess_the_number.py
@@ -9,11 +9,11 @@ attempt = 1
 
 # генерация случайного числа
 last_digit = 10
-random_digit = random.randint(1,last_digit)
+
 
 
 # Пользователь вводит уровень
-user_input_level = int(input('В каком диапазоне ты хочешь поискать цифру:\n1) от 1 до 10\n2) от 1 до 50\n3) от 1 до 100\n'))
+user_input_level = int(input('В каком диапазоне ты хочешь поискать цифру:\n1)от 1 до 10\n2)от 1 до 50\n3)от 1 до 100\n'))
 
 if user_input_level == 1:
     print("Отлично! Ты выбрал первый уровень - самый легкий. Он идеально подойдет для начала ✯")
@@ -23,23 +23,25 @@ elif user_input_level == 2:
     last_digit = 50
 elif user_input_level == 3:
     print("Самый сложный выбор! Держу за тебя кулачки - ✯✯✯")
-    last_digit = 101
+    last_digit = 100
 
-print(f"\n Программа загадала цифру от 1 до {last_digit}. Какую цифру ты введешь: ")
-print(random_digit)
+random_digit = random.randint(1,last_digit)
+print(f"\nПрограмма загадала цифру от 1 до {last_digit}. Какую цифру ты введешь: ")
+
 user_input_digit = int(input())
 
-while attempt != 5 or user_input_digit != random_digit:
+while attempt < 5 and user_input_digit != random_digit:
 
 
-    if user_input_level > random_digit:
+    if user_input_digit > random_digit:
         print('Твое число больше, попробуй поменьше')
         attempt = attempt + 1
-    elif user_input_level < random_digit:
+    elif user_input_digit < random_digit:
         print('Твое число меньше. Попробуй взять побольше')
         attempt = attempt + 1
     else:
         print('Ура! Ты угадал. Ты большой молодец!')
         print(f'Ты понадобилось {attempt} попыток')
+        break
 
     user_input_digit = int(input())
